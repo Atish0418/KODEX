7905c40bc90833c6cb76826a4180bddcd67e0ddf
Atish Adhav <adhavatish04@gmail.com>
2026-03-05 23:28:57 +0530

    bmi calculator

--- Daily Tsaks/Task1-BMI-calculator/script.js
+++ Daily Tsaks/Task1-BMI-calculator/script.js
@@ -1,37 +1,207 @@
-var startBtn = document.querySelector("#startBtn")
+var startBtn = document.querySelector("#startBtn");
 var backBtn = document.querySelector("#backBtn");
+var resultBackBtn = document.querySelector("#resultBackBtn");
+var calculateBtn = document.querySelector("#calculateBtn");
+
+
 var home = document.querySelector(".home");
 var fromPage = document.querySelector(".from-page");
+var resultPage = document.querySelector(".result-page");
 
-startBtn.addEventListener('click', function(){
-    home.style.display = 'none';
-    fromPage.style.display = 'flex';
-})
 
-backBtn.addEventListener('click', function(){
-    fromPage.style.display = 'none';
-    home.style.display = 'flex'
-})
+var nameInput = document.querySelector("#name");
+var addressInput = document.querySelector("#address");
+var dobInput = document.querySelector("#dob");
+var heightInput = document.querySelector("#height");
+var weightInput = document.querySelector("#weight");
 
-// swip back functionality
 
-var startX = 0;
-var endX = 0;
+var maleSelect = document.querySelector("#maleSelect");
+var femaleSelect = document.querySelector("#femaleSelect");
+var resultGenderImage = document.querySelector("#resultGenderImage");
 
-fromPage.addEventListener("touchstart", function(elem){
-    startX = elem.touches[0].clientX;
-})
+var selectedGender = "";
 
-fromPage.addEventListener("touchend", function(elem){
-    endX = elem.changedTouches[0].clientX;
+var resultName = document.querySelector("#resultName");
+var resultAddress = document.querySelector("#resultAddress");
+var resultBMI = document.querySelector("#resultBMI");
+var resultAge = document.querySelector("#resultAge");
+var resultHeight = document.querySelector("#resultHeight");
+var resultWeight = document.querySelector("#resultWeight");
+var resultCategory = document.querySelector("#resultCategory");
+var resultRange = document.querySelector("#resultRange");
+var resultDescription = document.querySelector("#resultDescription");
 
-    if(endX - startX > 80){
-        fromPage.style.display = 'none';
-        home.style.display = 'flex';
+
+
+window.onload = function () {
+
+    var savedData = JSON.parse(localStorage.getItem("bmiData"));
+
+    if (savedData) {
+
+        resultName.innerText = savedData.name;
+        resultAddress.innerText = savedData.address;
+        resultBMI.innerText = savedData.bmi;
+        resultAge.innerText = savedData.age + " yrs";
+        resultHeight.innerText = savedData.height + " cm";
+        resultWeight.innerText = savedData.weight + " kg";
+
+        resultCategory.innerText = savedData.category;
+        resultRange.innerText = savedData.range;
+        resultDescription.innerText = savedData.description;
+
+        if (savedData.gender === "male") {
+            resultGenderImage.src = "./assetes/male2.png";
+        } else {
+            resultGenderImage.src = "./assetes/female-2.png";
+        }
+
+        home.style.display = "none";
+        resultPage.style.display = "flex";
+    }
+};
+
+
+startBtn.onclick = function () {
+    home.style.display = "none";
+    fromPage.style.display = "flex";
+};
+
+backBtn.onclick = function () {
+    fromPage.style.display = "none";
+    home.style.display = "flex";
+};
+
+resultBackBtn.onclick = function () {
+    resultPage.style.display = "none";
+    fromPage.style.display = "flex";
+};
+
+
+
+maleSelect.onclick = function () {
+    selectedGender = "male";
+    maleSelect.classList.add("active");
+    femaleSelect.classList.remove("active");
+};
+
+femaleSelect.onclick = function () {
+    selectedGender = "female";
+    femaleSelect.classList.add("active");
+    maleSelect.classList.remove("active");
+};
+
+
+calculateBtn.onclick = function () {
+
+    var name = nameInput.value;
+    var address = addressInput.value;
+    var height = parseFloat(heightInput.value);
+    var weight = parseFloat(weightInput.value);
+    var dob = dobInput.value;
+
+    if (!name || !height || !weight || !dob || !selectedGender) {
+        alert("Please fill all fields");
+        return;
     }
 
-    if(startX - endX > 80){
-        fromPage.style.display = 'none';
-        home.style.display = 'flex';
+    var bmi = (weight / ((height / 100) * (height / 100))).toFixed(1);
+
+    var birth = new Date(dob);
+    var today = new Date();
+
+    var age = today.getFullYear() - birth.getFullYear();
+    var m = today.getMonth() - birth.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+        age--;
     }
-})
+
+    var category, range, description;
+
+    if (bmi < 18.5) {
+        category = "Underweight";
+        range = "(BMI less than 18.5)";
+        description = "Being underweight may indicate insufficient body fat.";
+    }
+    else if (bmi < 25) {
+        category = "Normal";
+        range = "(BMI 18.5 - 24.9)";
+        description = "You have a healthy body weight.";
+    }
+    else if (bmi < 30) {
+        category = "Overweight";
+        range = "(BMI 25 - 29.9)";
+        description = "You may be carrying extra weight.";
+    }
+    else {
+        category = "Obese";
+        range = "(BMI 30+)";
+        description = "High BMI may increase risk of health issues.";
+    }
+
+    resultName.innerText = name;
+    resultAddress.innerText = address;
+    resultBMI.innerText = bmi;
+    resultAge.innerText = age + " yrs";
+    resultHeight.innerText = height + " cm";
+    resultWeight.innerText = weight + " kg";
+
+    resultCategory.innerText = category;
+    resultRange.innerText = range;
+    resultDescription.innerText = description;
+
+    if (selectedGender === "male") {
+        resultGenderImage.src = "./assetes/male2.png";
+    } else {
+        resultGenderImage.src = "";
+    }
+
+
+    var bmiData = {
+        name: name,
+        address: address,
+        age: age,
+        height: height,
+        weight: weight,
+        bmi: bmi,
+        category: category,
+        range: range,
+        description: description,
+        gender: selectedGender
+    };
+
+    localStorage.setItem("bmiData", JSON.stringify(bmiData));
+
+    fromPage.style.display = "none";
+    resultPage.style.display = "flex";
+};
+
+let startX = 0;
+let endX = 0;
+
+function enableSwipe(page, backFunction) {
+
+    page.addEventListener("touchstart", function (e) {
+        startX = e.touches[0].clientX;
+    });
+
+    page.addEventListener("touchend", function (e) {
+        endX = e.changedTouches[0].clientX;
+
+        if (endX - startX > 80) {
+            backFunction();
+        }
+    });
+}
+
+enableSwipe(fromPage, function () {
+    fromPage.style.display = "none";
+    home.style.display = "flex";
+});
+
+enableSwipe(resultPage, function () {
+    resultPage.style.display = "none";
+    fromPage.style.display = "flex";
+});
