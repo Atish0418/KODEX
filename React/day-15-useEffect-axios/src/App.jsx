d50a8c2182aa92e5155fa5bad4c8ffd1c570d10d
Atish Adhav <adhavatish04@gmail.com>
2026-03-24 18:47:54 +0530

    learned useEffect 1 basics

---
+++ React/day-15-useEffect-axios/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import Home from './components/Home'
+import About from './components/About'
+
+const App = () => {
+
+  const [toggle, setToggle] = useState(false)
+  let [count, setCount] = useState(0)
+  console.log('app rerendering....')
+
+  useEffect(() => {
+    console.log('Hey I am Atish');
+  }, [count])
+
+  return (
+    <div>
+      <h1>App</h1>
+
+      {/* {toggle ? <Home/> : <About/>} */}
+
+      <Home/>
+
+      <button onClick={() => {
+        setToggle(prev => !prev)
+      }} 
+      >
+        Change
+      </button>
+
+      <button onClick={() => {
+        setCount(1);
+      }}
+      >
+        Increment
+      </button>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ React/day-15-useEffect-axios/src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+
+    console.log('About rerendering....')
+
+  return (
+    <div>
+        <h1>About</h1>
+    </div>
+  )
+}
+
+export default About
